0ac2c4185a6a302b24d1afb4e9fa112bf239dc5e
Andrea Raineri <ar.angelo259@gmail.com>
2021-10-25 16:02:08 +0200

    lab2 es3 completato

---
+++ LAB02/ES03/es03.c
@@ -0,0 +1,348 @@
+/* 
+    LAB2 ES03 RAINERI ANDREA ANGELO S280848
+    ALGORITMI E STRUTTURE DATI
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define FILENAME "corse.txt"
+#define MAX_LEN 30
+#define NUM_ORDS 4
+
+typedef struct corsa_s {
+    char codice[MAX_LEN];
+    char partenza[MAX_LEN], destinazione[MAX_LEN];
+    char data[MAX_LEN];
+    char partTime[MAX_LEN], arrivTime[MAX_LEN];
+    int ritardo;
+} corsa_t;
+
+enum comando_e { r_stampa, r_carica, r_ord_data, r_ord_codice, r_ord_partenza, r_ord_capolinea, r_ricerca, r_fine };
+enum ord_type_e { data, codice, partenza, capolinea };
+
+int readFile(corsa_t **corse, corsa_t ***corse_ord, int newRead);
+
+enum comando_e leggiComando();
+void eseguiComando(enum comando_e comando, corsa_t corse[], corsa_t **corse_ord[], int *numCorse);
+
+void formattaCorsa(corsa_t *corsa, char *str);
+void stampaLog(corsa_t *corse[], int numCorse);
+
+void ordinaCorse(corsa_t *corse[], int numCorse, int (*cmpFunction)(corsa_t corsa1, corsa_t corsa2));
+int confrontoData(corsa_t corsa1, corsa_t corsa2);
+int confrontoCodice(corsa_t corsa1, corsa_t corsa2);
+int confrontoPartenza(corsa_t corsa1, corsa_t corsa2);
+int confrontoCapolinea(corsa_t corsa1, corsa_t corsa2);
+
+void eseguiRicerca(corsa_t *corse[], int numCorse, int sorted);
+void ricercaLineare(corsa_t *corse[], int numCorse, char *input);
+void ricercaDicotomica(corsa_t *corse[], int numCorse, char *input);
+
+int main(int argc, char const *argv[]) {
+    enum comando_e comando;
+    int numCorse, i, j;
+    corsa_t *corse;
+    corsa_t **corse_ord[NUM_ORDS];
+    numCorse = readFile(&corse, corse_ord, 0);
+
+    do {
+        comando = leggiComando();
+        if (comando != r_fine)
+            eseguiComando(comando, corse, corse_ord, &numCorse);
+    } while (comando != r_fine);
+
+    for (i = 0; i < NUM_ORDS; i++) {
+        free(corse_ord[i]);
+    }
+    free(corse);
+
+    return 0;
+}
+
+int readFile(corsa_t **corse, corsa_t ***corse_ord, int newRead) {
+    FILE *fp;
+    int i, j;
+    int numCorse;
+    char fname[MAX_LEN];
+
+    if (newRead) {
+        printf("Inserire nome nuovo file: ");
+        scanf("%s", fname);
+        fp = fopen(fname, "r");
+    } else {
+        fp = fopen(FILENAME, "r");
+    }
+    if (fp == NULL) {
+        printf("ERRORE apertura file di input!");
+        exit(1);
+    }
+    fscanf(fp, "%d", &numCorse);
+
+    if (!newRead) {
+        for (i = 0; i < NUM_ORDS; i++) {
+            free(corse_ord[i]);
+        }
+        free(*corse);
+    }
+
+    *corse =  (corsa_t *) malloc(numCorse * sizeof(corsa_t));
+    for (i = 0; i < numCorse; i++) {
+        fscanf(fp, "%s %s %s %s %s %s %d", 
+            (*corse)[i].codice,
+            (*corse)[i].partenza,
+            (*corse)[i].destinazione,
+            (*corse)[i].data,
+            (*corse)[i].partTime,
+            (*corse)[i].arrivTime,
+            &(*corse)[i].ritardo );
+    }
+
+    fclose(fp);
+
+    for (i = 0; i < NUM_ORDS; i++) {
+        corse_ord[i] = (corsa_t**) malloc(numCorse * sizeof(corsa_t*));
+        for (j = 0; j < numCorse; j++) {
+            corse_ord[i][j] = &(*corse)[j];
+        }
+    }
+
+    return numCorse;
+}
+
+// Command reader
+enum comando_e leggiComando() {
+    int comandoOK = 0;
+    char input[MAX_LEN];
+
+    do {
+        printf("Indicare un comando da eseguire: ");
+        scanf("%s", input);
+
+        comandoOK = 1;
+        if (strcmp(input, "stampa") == 0) {
+            return r_stampa;
+        } else if (strcmp(input,  "carica") ==  0)  {
+            return r_carica;
+        } else if (strcmp(input, "ord_data") == 0) {
+            return r_ord_data;
+        } else if (strcmp(input, "ord_codice") == 0) {
+            return r_ord_codice;
+        } else if (strcmp(input, "ord_partenza") == 0) {
+            return r_ord_partenza;
+        } else if (strcmp(input, "ord_capolinea") == 0) {
+            return r_ord_capolinea;
+        } else if (strcmp(input, "ricerca") == 0) {
+            return r_ricerca;
+        } else if (strcmp(input, "fine") == 0) {
+            return r_fine;
+        } else {
+            printf("ERRORE: il comando inserito non esiste!\n");
+            comandoOK = 0;
+        }
+
+    } while (!comandoOK);
+}
+
+// Command handler
+void eseguiComando(enum comando_e comando, corsa_t corse[], corsa_t **corse_ord[], int *numCorse) {
+    static enum ord_type_e ord_type;
+    static int depart_ordered = 0;
+
+    switch (comando) {
+        case r_stampa:
+            stampaLog(corse_ord[ord_type], *numCorse);
+            break;
+        case r_carica:
+            *numCorse = readFile(&corse, corse_ord, 1);
+            break;
+        case r_ord_data:
+            ordinaCorse(corse_ord[data], *numCorse, confrontoData);
+            ord_type = data;
+            break;
+        case r_ord_codice:
+            ordinaCorse(corse_ord[codice], *numCorse, confrontoCodice);
+            ord_type = codice;
+            break;
+        case r_ord_partenza:
+            ordinaCorse(corse_ord[partenza], *numCorse, confrontoPartenza);
+            ord_type = partenza;
+            depart_ordered = 1;
+            break;
+        case r_ord_capolinea:
+            ordinaCorse(corse_ord[capolinea], *numCorse, confrontoCapolinea);
+            ord_type = capolinea;
+            break;
+        case r_ricerca:
+            // Search always on the array ordered by departure, if it has actually been ordered the search will be dicotomic
+            eseguiRicerca(corse_ord[partenza], *numCorse, depart_ordered);
+            break;
+        default:
+            break;
+    }
+}
+
+void formattaCorsa(corsa_t *corsa, char *str) {
+    sprintf(str, "%s %s %s %s %s %s %d\n", 
+        corsa->codice,
+        corsa->partenza,
+        corsa->destinazione,
+        corsa->data,
+        corsa->partTime,
+        corsa->arrivTime,
+        corsa->ritardo
+    );
+}
+
+void stampaLog(corsa_t *corse[], int numCorse) {
+    FILE *fp;
+    int i, opt, validInput = 0;
+    char filename[MAX_LEN], str[MAX_LEN*10];
+
+    do {
+        printf("Stampa a video (0) o su file (1 <nome_file>): ");
+        scanf("%d", &opt);
+
+        if (opt == 0) {
+            validInput = 1;
+        } else if ((opt == 1) && scanf("%s", filename) == 1) {
+            validInput = 1;
+        } else {
+            validInput = 0;
+        }
+    } while (!validInput);
+    
+    if (opt == 0) {
+        for (i = 0; i < numCorse; i++) {
+            formattaCorsa(corse[i], str);
+            printf("%s", str);
+        }
+    } else {
+        if ((fp = fopen(filename, "w")) == NULL) {
+            printf("ERRORE creazione file");
+            exit(1);
+        }
+
+        for (i = 0; i < numCorse; i++) {
+            formattaCorsa(corse[i], str);
+            fprintf(fp, "%s", str);
+        }
+
+        fclose(fp);
+    }
+    
+}
+
+// Insertion Sort implementation
+void ordinaCorse(corsa_t *corse[], int numCorse, int (*cmpFunction)(corsa_t corsa1, corsa_t corsa2)) {
+    int i, j, l=0, r=numCorse-1;
+    corsa_t *x;
+
+    for (i = l+1; i <= r; i++) {
+        x = corse[i];
+        for (j = i-1; j >= l && (*cmpFunction)(*x, *corse[j]) < 0; j--) {
+            corse[j+1] = corse[j];
+        }
+        corse[j+1] = x;
+    }
+}
+
+/* 
+    Compare functions for sorting
+ */
+// Sort for date/departure time
+int confrontoData(corsa_t corsa1, corsa_t corsa2) {
+    int res;
+    if ((res = strcmp(corsa1.data, corsa2.data)) == 0) {
+        return strcmp(corsa1.partTime, corsa2.partTime);
+    } else {
+        return res;
+    }
+}
+
+// Sort for code
+int confrontoCodice(corsa_t corsa1, corsa_t corsa2) {
+    return strcmp(corsa1.codice, corsa2.codice);
+}
+
+// Sort for departure
+int confrontoPartenza(corsa_t corsa1, corsa_t corsa2) {
+    return strcmp(corsa1.partenza, corsa2.partenza);
+}
+
+// Sort for arrival
+int confrontoCapolinea(corsa_t corsa1, corsa_t corsa2) {
+    return strcmp(corsa1.destinazione, corsa2.destinazione);
+}
+
+// Search handler
+void eseguiRicerca(corsa_t *corse[], int numCorse, int sorted) {
+
+    char input[MAX_LEN];
+    int idx;
+
+    printf("Inserire nome (o parziale) della stazione di partenza: ");
+    scanf("%s", input);
+
+    if (!sorted) {
+        printf("I dati non risultano ordinati, effettuo una ricerca lineare\n");
+        ricercaLineare(corse, numCorse, input);
+    } else {
+        printf("I dati risultano ordinati, effettuo una ricerca dicotomica\n");
+        ricercaDicotomica(corse, numCorse, input);
+    }
+}
+
+void ricercaLineare(corsa_t *corse[], int numCorse, char *input) {
+    int i, found = 0;
+    char str[MAX_LEN*10];
+    for (i = 0; i < numCorse; i++) {
+        if (strncmp(corse[i]->partenza, input, strlen(input)) == 0) {
+            formattaCorsa(corse[i], str);
+            printf("Posizione: %d -- %s", i, str);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Nessun dato trovato\n");       
+    }
+}
+
+void ricercaDicotomica(corsa_t *corse[], int numCorse, char *input) {
+    int found = 0, idx, l=0, r=numCorse-1, cmp;
+    int i;
+    char str[MAX_LEN*10];
+
+    while (!found && (l <= r)) {
+        idx = (r+l)/2;
+        cmp = strncmp(corse[idx]->partenza, input, strlen(input));
+        if (cmp == 0) {
+            found = 1;
+        } else if (cmp > 0) {
+            r = idx - 1;
+        } else {
+            l = idx + 1;
+        }
+    }
+    
+    if (found) {
+        formattaCorsa(corse[idx], str);
+        printf("Posizione: %d -- %s", idx, str);
+
+        i = idx + 1;
+        while((i < numCorse) && (strncmp(corse[i]->partenza, input, strlen(input)) == 0)) {
+            formattaCorsa(corse[i], str);
+            printf("Posizione: %d -- %s", i, str);
+            i++;
+        }
+        i = idx - 1;
+        while((i >= 0) && (strncmp(corse[i]->partenza, input, strlen(input)) == 0)) {
+            formattaCorsa(corse[i], str);
+            printf("Posizione: %d -- %s", i, str);
+            i--;
+        }
+    } else {
+        printf("Nessun dato trovato\n");
+    }
+    
+}
