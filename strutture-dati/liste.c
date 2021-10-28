096d3fb593d98815b55ad931fce1b2bc9c4b77dc
Andrea Raineri <ar.angelo259@gmail.com>
2021-10-28 15:52:31 +0200

    node insertion at head functions

--- strutture-dati/liste.c
+++ strutture-dati/liste.c
@@ -18,10 +18,10 @@ struct node {
 };
 
 /** 
- *   * New node function
- *   @param Item Data of the new node
- *   @param link Pointer to the next node
- *   @return Pointer to the new node
+ *  * New node function
+ *  @param Item Data of the new node
+ *  @param link Pointer to the next node
+ *  @return Pointer to the new node
  */
 link newNode(Item val, link next) {
     link t;
@@ -38,4 +38,28 @@ link newNode(Item val, link next) {
     // Return a pointer to the new node
     return t;
     
+}
+
+/**
+ *  * Insert node at head of list
+ *  @param Item Data of the node
+ *  @param link Head of the list
+ *  @return Pointer to the new head of the list
+ */
+link insertNodeAtHead(Item val, link head) {
+
+    return newNode(val, head);
+
+}
+
+/**
+ *  * Insert node at head of list (by pointer)
+ *  @param Item Data of the node
+ *  @param link* Pointer to head of the list
+ *  @return --
+ */
+void insertNodeAtHeadP(Item val, link *head) {
+
+    *head = newNode(val, *head);
+
 }
