89f3e69b6aaa158a14561cd339bfbfe2082e2328
Andrea Raineri <ar.angelo259@gmail.com>
2021-11-04 11:13:28 +0100

    algoritmi ricorsivi: binary search

--- algoritmi/ricorsione.c
+++ algoritmi/ricorsione.c
@@ -43,10 +43,40 @@ int maxR(int A[], int l, int r) {
         return m1;
 }
 
+/**
+ *  * Binary Search
+ *  @param int[] array to search for element
+ *  @param int left index of the subvector
+ *  @param int right index of the subvector
+ *  @param int element to search
+ *  @return int boolean indicating the success of the search
+ */
+int binarySearch(int array[], int l, int r, int k) {
+    int q = (l+r)/2;
+
+    // Terminal condition: array of dim 1, compare element with key
+    if (l == r) {
+        if (array[l] == k)
+            return 1;
+        else
+            return 0;
+    }
+
+    // Recursive calls
+    if (array[q] == k) {
+        return 1;
+    } else if (array[q] > k) {
+        return binarySearch(array, l, q-1, k);
+    } else {
+        return binarySearch(array, q+1, r, k);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int V[N_dim] = {1, 34, 2, 45, 6, 7, 8, 98, 124, 2};
     printf("Max: %d\n", max(V, N_dim));
+    printf("%d\n", binarySearch(V, 0, N_dim-1, 124));
     
     return 0;
 }
