f1e346a0c1adb199fb15d91b0fd2cd86579881dd
Andrea Raineri <ar.angelo259@gmail.com>
2021-11-04 09:41:20 +0100

    implementazione quick sort

--- algoritmi/sorting.c
+++ algoritmi/sorting.c
@@ -69,3 +69,69 @@ void Merge(int A[], int B[], int l, int q, int r) {
     for (k = l; k <= r; k++)
         A[k] = B[k];
 }
+
+/**
+ *  * QUICK SORT WRAPPER: 
+ *  * Hides the recursive implementation of the algorithm to the client
+ *  @param Item[] array to order (size N)
+ *  @param int size N of the array (number of items to order)
+ *  @return --
+ */
+void QuickSort(int A[], int N) {
+    int l=0, r=N-1;
+    QuickSortR(A, l, r);
+}
+
+/**
+ *  * QUICK SORT (recursive call)
+ *  @param Item[] array of items to order
+ *  @param int left index of the subvector to order
+ *  @param int right index of the subvector to order
+ *  @return --
+ */
+void QuickSortR(int A[], int l, int r) {
+    int q;
+    
+    // Terminal condition: subvector of dim <=1 already ordered
+    if (l >= r)
+        return;
+
+    // Divide
+    q = partition(A, l, r);
+
+    // Recursive calls on subproblems
+    QuickSortR(A, l, q-1);
+    QuickSortR(A, q+1, r);
+}
+/**
+ *  * Partition function called by Quick Sort, divides the vector into two subvectors with a pivot element in its final position q
+ *  @param Item[] array of items to divide
+ *  @param int left index of the subvector to divide
+ *  @param int right index of the subvector to divide
+ * @return int index of the pivot element the vector is divided by
+ */
+int partition(int A[], int l, int r) {
+    int i = l-1, j = r;
+    int tmp;
+    int x = A[r];
+
+    for (; ;) {
+        while(A[++i] < x);
+        while(A[--j] > x)
+            if (j == l)
+                break;
+        if (i >= j)
+            break;
+
+        // Swap to position elements into the two subvectors
+        tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+    // Positioning trhe pivot element in its final position
+    tmp = A[i];
+    A[i] = A[r];
+    A[r] = tmp;
+    
+    return i;
+}
